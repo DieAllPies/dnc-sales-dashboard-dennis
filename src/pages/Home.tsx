ecead3abdfe1ac3e7273d1a275db5292a5d9d50e
DieAllPies <dennisfamiliapedersen@gmail.com>
2025-06-05 15:45:48 -0300

    Feat: Avatars list component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,34 @@
-import { CardComponent, Header } from '@/components'
+import { AvatarsList, CardComponent, Header } from '@/components'
 import { Container } from '@mui/material'
+import { currencyConverter } from '@/utils'
 
 function Home() {
+  const mockListData = [
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 1',
+      subtitle: currencyConverter(6242.64),
+    },
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 2',
+      subtitle: currencyConverter(3246.32),
+    },
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 3',
+      subtitle: currencyConverter(2346.84),
+    },
+  ]
   return (
     <>
       <Header />
       <Container maxWidth="lg">
         <CardComponent>Card</CardComponent>
+        <CardComponent>
+          <AvatarsList listData={mockListData} />
+        </CardComponent>
       </Container>
-      <h1>Home</h1>
     </>
   )
 }
